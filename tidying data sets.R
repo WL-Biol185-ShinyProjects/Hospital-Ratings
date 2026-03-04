83c7b785adefcbed25282bf0a3d35a76ccd62a2b
Joanne Lee <jlee2@mail.wlu.edu>
2026-03-04 12:49:44 -0500

    saving cleanup so far

--- tidying data sets.R
+++ tidying data sets.R
@@ -13,12 +13,10 @@ VA_IPF <- read.csv("VA_IPF_cleaned.csv")
 getwd()
 ls()
 write.csv(VA_IPF, "VA_IPF_cleaned.csv", row.names = FALSE)
-Timely_and_Effective_Care_Hospital <- 
 #Tidying Timely and Effect...
   #none of this is in the shared file pls figure this out next time!!
 TimelyEffectiveCare <- Timely_and_Effective_Care_Hospital
-
 TimelyEffectiveCare[["Facility ID"]] <- NULL
 colnames(TimelyEffectiveCare)
 TimelyEffectiveCare[["Facility ID"]] <- NULL
-bello
+
