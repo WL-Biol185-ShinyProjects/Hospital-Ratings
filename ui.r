b3843091b0b9c0271c8880ac4efb7813a340d30f
Maren Barclay <mbarclayl@mail.wlu.edu>
2026-03-04 14:09:29 -0500

    making UI framework and labels

--- ui.r
+++ ui.r
@@ -1,7 +1,7 @@
 library(markdown)
 
-navbarPage("Navbar!",
-           tabPanel("Plot",
+navbarPage("Hospital Ratings",
+           tabPanel("Directory Map",
                     sidebarLayout(
                       sidebarPanel(
                         radioButtons("plotType", "Plot type",
@@ -13,32 +13,20 @@ navbarPage("Navbar!",
                       )
                     )
            ),
-           tabPanel("Summary",
+           tabPanel("Star Ratings",
                     verbatimTextOutput("summary")
            ),
-           navbarMenu("More",
+           tabPanel("Staff and Communication"),
+           tabPanel("Risk Factors"),
+           
+             navbarMenu("More",
                       tabPanel("Table",
                                DT::dataTableOutput("table")
                       ),
                       tabPanel("About",
-                               fluidRow(
-                                 column(6,
-                                        includeMarkdown("about.md")
-                                 ),
-                                 column(3,
-                                        img(class="img-polaroid",
-                                            src=paste0("http://upload.wikimedia.org/",
-                                                       "wikipedia/commons/9/92/",
-                                                       "1919_Ford_Model_T_Highboy_Coupe.jpg")),
-                                        tags$small(
-                                          "Source: Photographed at the Bay State Antique ",
-                                          "Automobile Club's July 10, 2005 show at the ",
-                                          "Endicott Estate in Dedham, MA by ",
-                                          a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
-                                            "User:Sfoskett")
+                               
                                         )
                                  )
                                )
-                      )
-           )
-)
+
+
