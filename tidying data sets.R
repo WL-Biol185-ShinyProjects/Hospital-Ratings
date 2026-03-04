4527b61984f63c6e747f0a1b9bedc041492d20fb
Joanne Lee <jlee2@mail.wlu.edu>
2026-03-04 13:16:31 -0500

    saving new changed vp ipf file

--- tidying data sets.R
+++ tidying data sets.R
@@ -1,14 +1,23 @@
-# Tidying VA IPF
 library(tidyverse)
-VA_IPF[["Facility ID"]] <-NULL
-VA_IPF[["Start Date"]] <-NULL
-VA_IPF[["End Date"]] <-NULL
+#seeing what's in environment
+ls()
+environmentName(environment())
+#tidying va ipf
+VA_IPF[["Facility.ID"]] <-NULL
+VA_IPF[["Start.Date"]] <-NULL
+VA_IPF[["End.Date"]] <-NULL
+VA_IPF[["Sample"]] <- NULL
+VA_IPF[["Footnote"]] <- NULL
+
+#what's this??
 VA_IPF <- read.csv("VA_IPF.csv")
-VA_IPF[["Facility ID"]] <- NULL
-VA_IPF[["Start Date"]] <- NULL
-VA_IPF[["End Date"]] <- NULL
-write.csv(VA_IPF, "VA_IPF.csv", row.names = FALSE)
+#saving file as new
+write.csv(VA_IPF, "VA_IPF_cleaned.csv", row.names = FALSE)
 VA_IPF <- read.csv("VA_IPF_cleaned.csv")
+
+#fixing errors:
+names(VA_IPF)
+
 
 getwd()
 ls()
