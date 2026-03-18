f3a1199b4ed9e8609537ee688fb6a10b89b04618
Maren Barclay <mbarclayl@mail.wlu.edu>
2026-03-18 13:14:42 -0400

    risk factor stuff again

--- ui.r
+++ ui.r
@@ -68,58 +68,64 @@ navbarPage("Hospital Ratings",
                       tabPanel("Surgery Centers")
            ),
           #RISK FACTORS
-            tabPanel("Risk Factors",
-                    tabsetPanel(
-                      tabPanel("Risk Factors",
-                      # Step 1: Guided inputs
-                      fluidRow(
-                        column(4, selectInput("state_hai", "What state are you in?",
-                                              choices = c("All", sort(unique(hai_cleaned$State))),
-                                              selected = "All")),
-                        column(4, selectInput("facility_hai", "Select a Hospital:",
-                                              choices = c("Select a hospital..." = ""),
-                                              selected = "")),
-                        column(4, selectInput("care_type", "What type of care are you seeking?",
-                                              choices = c("General",
-                                                          "Surgery",
-                                                          "Maternity",
-                                                          "Emergency")))
-                      ),
-                      
-                      hr(),
-                      
-                      # Step 2: Hospital card (renders from server once facility is chosen)
-                      uiOutput("hospital_card"),
-                      
-                      # Step 3: Compare nudge (renders from server once facility is chosen)
-                      uiOutput("compare_nudge")
-                    ),
-                      tabPanel("Compare Hospitals",
-                               br(),
-                               fluidRow(
-                                 column(3, selectInput("state_hai_compare", "Filter by State:",
-                                                       choices = c("All", sort(unique(hai_cleaned$State))),
-                                                       selected = "All")),
-                                 column(9, checkboxGroupInput("infection_type_compare", "Select Infection Types:",
-                                                              choices = c("Central Line Infection",
+          tabPanel("Risk Factors",
+                   tabsetPanel(
+                     id = "risk_tabs",
+                     
+                     tabPanel("Find My Hospital",
+                              br(),
+                              # Step 1: Guided inputs
+                              fluidRow(
+                                column(4, selectInput("state_hai", "What state are you in?",
+                                                      choices = c("All", sort(unique(hai_cleaned$State))),
+                                                      selected = "All")),
+                                column(4, selectInput("facility_hai", "Select a Hospital:",
+                                                      choices = c("Select a hospital..." = ""),
+                                                      selected = "")),
+                                column(4, selectInput("care_type", "What type of care are you seeking?",
+                                                      choices = c("General",
+                                                                  "Surgery",
+                                                                  "Maternity",
+                                                                  "Emergency")))
+                              ),
+                              
+                              hr(),
+                              
+                              # Step 2: Hospital card
+                              uiOutput("hospital_card"),
+                              
+                              # Step 3: Compare nudge
+                              uiOutput("compare_nudge")
+                     ),
+                     
+                     tabPanel("Compare Hospitals",
+                              br(),
+                              fluidRow(
+                                column(3, selectInput("state_hai_compare", "Filter by State:",
+                                                      choices = c("All", sort(unique(hai_cleaned$State))),
+                                                      selected = "All")),
+                                column(9, checkboxGroupInput("infection_type_compare", "Select Infection Types:",
+                                                             choices = c("Central Line Infection",
+                                                                         "Urinary Tract Infection",
+                                                                         "Surgical Site - Colon",
+                                                                         "Surgical Site - Hysterectomy",
+                                                                         "MRSA Blood Infection",
+                                                                         "C. Difficile Infection"),
+                                                             selected = c("Central Line Infection",
                                                                           "Urinary Tract Infection",
-                                                                          "Surgical Site - Colon",
-                                                                          "Surgical Site - Hysterectomy",
                                                                           "MRSA Blood Infection",
                                                                           "C. Difficile Infection"),
-                                                              selected = c("Central Line Infection",
-                                                                           "Urinary Tract Infection",
-                                                                           "MRSA Blood Infection",
-                                                                           "C. Difficile Infection"),
-                                                              inline = TRUE))
-                               ),
-                               hr(),
-                               fluidRow(column(12, plotOutput("hai_compare_chart", height = "500px")))
-                      )
-                    )
-           ),
-           navbarMenu("More",
-                      tabPanel("Table", DT::dataTableOutput("table")),
-                      tabPanel("About")
-           )
+                                                             inline = TRUE))
+                              ),
+                              hr(),
+                              fluidRow(column(12, plotOutput("hai_compare_chart", height = "500px")))
+                     )
+                     
+                   ) # end tabsetPanel
+          ), # end Risk Factors tabPanel
+          
+          navbarMenu("More",
+                     tabPanel("Table", DT::dataTableOutput("table")),
+                     tabPanel("About")
+          )
 )
