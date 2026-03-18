d3ea60ae4fef8ae2b7f408b754775e256811f51c
Joanne Lee <jlee2@mail.wlu.edu>
2026-03-18 14:45:24 -0400

    icons

--- surg_directory_geocode.R
+++ surg_directory_geocode.R
@@ -17,13 +17,32 @@ SurgMap <-
     addMarkers(
       lng   = ~longitude,
       lat   = ~latitude,
+      icon  = Icon
+    )
       popup = ~paste0(
         "<b>", `Facility Name`, "</b><br>",
         full_address
       )
-    )
+
+
 #to view:
 SurgMap
+
+#icon:
+emoji <- "🏥"
+
+Icon <- makeIcon(
+  iconUrl = paste0(
+    "data:image/svg+xml,",
+    URLencode(paste0(
+      "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'>",
+      "<text y='.9em' font-size='90'>", emoji, "</text>",
+      "</svg>"
+    ))
+  ),
+  iconWidth  = 35,
+  iconHeight = 35
+)
 
 #opening and saving new data set- needto finish
 View(locations)
@@ -32,3 +51,8 @@ file.rename("locations.csv", "surg_centers")
 
 
 #CODING DIRECTORY
+#obtaining coordinates(didn't work cause it took 2 hours? Figure something else out)
+Hospital_locations <- Hospital_General_Information_1_ %>%
+  unite("full_address", `Address`, `City/Town`, `State`, `ZIP Code`,
+        sep = ", ", remove = FALSE, na.rm = TRUE) %>%
+  geocode(full_address, method = "osm", lat = latitude, long = longitude)
